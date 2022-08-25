4802ea78892a07218838f48630987672f49b3c42
Kenny <vercore5866@gmail.com>
2022-08-25 06:46:26 -0400

    Added Concrete Algebraic Optics Example

---
+++ AlgebraicOptics/Domain.fs
@@ -0,0 +1,11 @@
+﻿module Domain
+    open FSharpPlus
+    open Prelude.Concrete 
+
+    type Species = Setsoa | Versicolor | Virginica
+    type Measurements = { getMeasurements : float list}
+    type Flower = { flowerSpecies : Species; flowerMeasurements: Measurements}
+
+    let zipWith (f : 'a -> 'b -> 'c) (a: 'a list) (b : 'b list) =
+        zip a b
+        |> fold (fun (state: 'c list) (a: 'a,  b: 'b) -> [f a b] @ state) []
---
+++ AlgebraicOptics/Program.fs
@@ -0,0 +1,34 @@
+﻿module Program = 
+    open Prelude.Concrete
+    open Domain
+    open FSharpPlus
+
+    // Classification functions
+    let measurementDistance (xs : Measurements) (ys: Measurements) : float =
+        let diff a b = (a - b) * 2.0
+        (zipWith diff xs.getMeasurements ys.getMeasurements)
+        |> (sqrt << sum)
+
+    let classify (flowers : Flower list) (measurements : Measurements)  =
+        match flowers with
+        | [] -> None
+        | flowers -> 
+            let flower = Seq.minBy (fun (f : Flower) -> measurementDistance f.flowerMeasurements measurements) flowers
+            Some({flowerSpecies = flower.flowerSpecies; flowerMeasurements = measurements})
+    
+    // Algebraic Lens carrying the algebra for the product of the list monad 
+    let flowerLens = AlgebraicLens ((fun (f: Flower) -> f.flowerMeasurements), classify)
+
+    // Sample Data
+    let versicolor = {flowerSpecies = Versicolor; flowerMeasurements = { getMeasurements = [2;3;4;2] } }
+    let setosa = {flowerSpecies = Setsoa; flowerMeasurements = { getMeasurements = [5;4;3;2.5] } }
+    let flowers = [versicolor; setosa]
+
+    // Viewing the Measurements of a given flower sample set
+    let test = flowerLens.view (flowers.[0])
+
+    // Classifying a new flower based off of sample data
+    let test2 = flowerLens.classify flowers { getMeasurements = [1.9; 3.2; 3.8; 2]}
+
+    printfn "%A" test   // { getMeasurements = [2.0; 3.0; 4.0; 2.0] }
+    printfn "%A" test2  // Some { flowerSpecies = Versicolor flowerMeasurements = { getMeasurements = [1.9; 3.2; 3.8; 2.0] } }
